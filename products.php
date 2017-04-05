1305c13011125878b1d69af5fad8406a81c53119
Mark-Sixma <AhmedSixma@gmail.com>
2017-04-05 18:12:59 +0200

    r2-Products feature added

---
+++ products.php
@@ -0,0 +1,59 @@
+<?php
+
+  include("login.php");
+
+  include("connection.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>login Page</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <script   src="https://code.jquery.com/jquery-1.12.4.js"   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="   crossorigin="anonymous"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+    
+  <body>
+    
+    <div>
+        <p>First Release...</p>
+    </div>
+	
+	<div class = "products"> 
+		<ul>
+			<li>Accessories</li>
+			<li>Mobiles</li>
+			<li>LapTops</li>
+			<li>TVs</li>
+		</ul>
+	</div>
+
+
+    <script type="text/javascript">
+    
+      $("#topContainer").css("min-height",$(window).height());
+
+
+    </script> 
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
